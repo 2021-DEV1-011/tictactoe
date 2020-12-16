020ebbf0ddc71fb01ec65959e98d69192d26a82d
2021-DEV1-011 <prateekagrawal610@gmail.com>
2020-12-16 21:09:12 +0530

    REFACTOR: introducing before-each to avoid duplicate operation

--- src/test/java/com/bnpp/kata/tictactoe/impl/TicTacToeTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/impl/TicTacToeTest.java
@@ -1,6 +1,7 @@
 package com.bnpp.kata.tictactoe.impl;
 
 import com.bnpp.kata.tictactoe.exception.InvalidInputException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,16 @@ import java.io.ByteArrayInputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeTest {
+    TicTacToe ticTacToe;
+
+    @BeforeEach
+    public void initialSetup() {
+        ticTacToe = new TicTacToe();
+    }
 
     @Test
     @DisplayName("Should initialise tic tac toe board and winner")
     public void initialiseGame() {
-        TicTacToe ticTacToe = new TicTacToe();
         assertNull(ticTacToe.getWinner());
         assertNotNull(ticTacToe.getBoard());
     }
@@ -21,10 +27,9 @@ class TicTacToeTest {
     @Test
     @DisplayName("Should throw InvalidInputException on invalid input while playing")
     public void throwExceptionWhilePlaying() {
-        TicTacToe game = new TicTacToe();
         System.setIn(new ByteArrayInputStream("10".getBytes()));
         try {
-            game.play();
+            ticTacToe.play();
         } catch (Exception exception) {
             assertSame(InvalidInputException.class,exception.getClass());
             assertSame("Position entered is not valid",exception.getMessage());
