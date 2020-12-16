89de297ad16760d6f9b2a8f97f771dd68ba82365
2021-DEV1-011 <prateekagrawal610@gmail.com>
2020-12-16 21:56:13 +0530

    REFACTOR: replacing string value with constant

--- src/main/java/com/bnpp/kata/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/bnpp/kata/tictactoe/TicTacToeApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.logging.Logger;
 
+import static com.bnpp.kata.tictactoe.constant.GameConstant.GAME_RULES;
+
 @SpringBootApplication
 public class TicTacToeApplication {
 	private static final Logger logger = Logger.getLogger(TicTacToeApplication.class.getName());
@@ -25,14 +27,7 @@ public class TicTacToeApplication {
 	}
 
 	private static String displayRules() {
-		return "Welcome to BNPPF KATA Tic Tac Toe\n" +
-				"Rules for this game are as follows: \n" +
-				"1. Player 1: X always goes first \n" +
-				"2. Players cannot play on a played position\n" +
-				"3. Players can play alternatively until one of them WINS the game or it leads to a DRAW\n" +
-				"4. A Player is said to WIN only if they have 3 X's or O's in a row completed\n" +
-				"5. Game is said to be DRAW if the players exhausts all the 9 positions\n" +
-				"6. You need to input your position starting from 1 till 9, where 1 is top left corner and 9 being bottom right corner";
+		return GAME_RULES;
 	}
 
 }
--- src/main/java/com/bnpp/kata/tictactoe/constant/GameConstant.java
+++ src/main/java/com/bnpp/kata/tictactoe/constant/GameConstant.java
@@ -36,4 +36,13 @@ public class GameConstant {
     public static final String WINNER_MESSAGE = " player Wins!!!";
     public static final String GAME_OVER_MESSAGE = "It's a DRAW!!! All the moves have been exhausted";
 
+    public static final String GAME_RULES = "Welcome to BNPPF KATA Tic Tac Toe\n" +
+            "Rules for this game are as follows: \n" +
+            "1. Player 1: X always goes first \n" +
+            "2. Players cannot play on a played position\n" +
+            "3. Players can play alternatively until one of them WINS the game or it leads to a DRAW\n" +
+            "4. A Player is said to WIN only if they have 3 X's or O's in a row completed\n" +
+            "5. Game is said to be DRAW if the players exhausts all the 9 positions\n" +
+            "6. You need to input your position starting from 1 till 9, where 1 is top left corner and 9 being bottom right corner";
+
 }
