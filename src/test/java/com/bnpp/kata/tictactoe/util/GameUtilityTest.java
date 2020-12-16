7aea9e138ba3ada66ac3b405959422c8c2ad723f
2021-DEV1-011 <prateekagrawal610@gmail.com>
2020-12-16 19:51:17 +0530

    REFACTOR: introducing parameterized test cases for validating input

--- src/test/java/com/bnpp/kata/tictactoe/util/GameUtilityTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/util/GameUtilityTest.java
@@ -1,21 +1,19 @@
 package com.bnpp.kata.tictactoe.util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameUtilityTest {
 
-    @Test
-    @DisplayName("Should take position one as a valid input")
-    void isPositionOneValidInput() {
-        assertTrue(GameUtility.isValidInput("1"));
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2"})
+    @DisplayName("Should take position one, two as a valid input")
+    void isValidInput(String position) {
+        assertTrue(GameUtility.isValidInput(position));
     }
 
-    @Test
-    @DisplayName("Should take position two as a valid input")
-    void isPositionTwoValidInput() {
-        assertTrue(GameUtility.isValidInput("2"));
-    }
 }
