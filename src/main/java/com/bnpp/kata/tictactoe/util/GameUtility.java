d6afc031bfbdde2a0c9fc348af1ac0ec9550a84f
2021-DEV1-011 <prateekagrawal610@gmail.com>
2020-12-16 20:05:13 +0530

    ADDED: position played is occupied or not

--- src/main/java/com/bnpp/kata/tictactoe/util/GameUtility.java
+++ src/main/java/com/bnpp/kata/tictactoe/util/GameUtility.java
@@ -1,11 +1,16 @@
 package com.bnpp.kata.tictactoe.util;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static com.bnpp.kata.tictactoe.constant.GameConstant.*;
 
 public class GameUtility {
     static boolean isValidInput(String input) {
         return Arrays.asList(new String[]{POSITION_ONE, POSITION_TWO, POSITION_THREE, POSITION_FOUR, POSITION_FIVE, POSITION_SIX, POSITION_SEVEN, POSITION_EIGHT, POSITION_NINE}).contains(input);
+    }
+
+    static boolean isPositionOccupied(List<String> movesPlayed, String position){
+        return movesPlayed.contains(position);
     }
 }
--- src/test/java/com/bnpp/kata/tictactoe/util/GameUtilityTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/util/GameUtilityTest.java
@@ -1,9 +1,13 @@
 package com.bnpp.kata.tictactoe.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,4 +20,13 @@ class GameUtilityTest {
         assertTrue(GameUtility.isValidInput(position));
     }
 
+    @Test
+    @DisplayName("Should check if position played is occupied or not")
+    void isPositionOccupied() {
+        List<String> movesPlayed = new ArrayList<>();
+        movesPlayed.add("1");
+        movesPlayed.add("2");
+        assertTrue(GameUtility.isPositionOccupied(movesPlayed,"1"));
+        assertFalse(GameUtility.isPositionOccupied(movesPlayed,"3"));
+    }
 }
